1246cc34c2d1ce7b29194ad709d502e0419cdfb3
Rebecca Jolitz <rjolitz@berkeley.edu>
2025-10-28 12:41:15 -0700

    (anc) New features + comments for anc: 'segment_orbit' returns the center orbitnumber, Unix time, and UTC time AND, if dorb != 0, the orbitnum, Unix time, and UTC time at the beginning and end of the orbit spacing; useful for determining time periods of continuous coverage. 'sort_segments_by_day' returns a dictionary with day indices such that the orbit segments are continuously covered within the set of loaded days; useful for continuous coverage analysis. Ideally, should be expanded to determine continuous coverage periods for particular conditions/altitude/Ls times.'

--- mavenpy/anc.py
+++ mavenpy/anc.py
@@ -9,9 +9,10 @@ import numpy as np
 import matplotlib.dates
 
 from .helper import find_closest_index_dt, daterange,\
-    UNX_to_UTC, UTC_to_UNX
+    UNX_to_UTC, UTC_to_UNX, sanitize_date_inputs
 from .read import read_sav
 from .retrieve import download_file, Spinner
+from .specification import in_datagap
 from . import spice
 
 # Routines for accessing ancillary MAVEN data,
@@ -368,3 +369,330 @@ def add_orbit_axis(ax, data_directory=None, ephemeris=None,
         secax.set_xlabel(label)
 
     return secax
+
+
+# Routines to retrieve / sort sections of an orbit.
+# These were developed to do smart loading of parts of a
+# dataset.
+
+# 'segment_orbit' will load the orbit ephemeris and return
+# a dictionary containing the Unix time, UTC time, and orbit number
+# for the center (+ start and end) of an orbit segment
+# given the string description of the center (apoapse or periapse)
+# and width of the orbit segment (e.g. 0.15).
+
+# 'sort_segments_by_day' will identify periods (# of days)
+# where orbit segments are completely enclosed.
+# e.g. if getting 15% of orbits within periapse and
+# an orbit segment is continuing onto the next day,
+# add a day and check again if orbit segment stretching to next day.
+# Repeat until the orbit segment doesn't cross the day boundary.
+
+# This is particularly useful for a mission-long analysis that
+# is focused on specific orbit segments, since it means you can
+# load a few days at a time instead of an entire dataset into
+# memory (at which point, just do a day by day analysis).
+
+
+def segment_orbit(data_directory, start_time, end_time, dorb,
+                  center_on='apoapse',
+                  download_if_not_available=None,
+                  t_format_string='%Y-%m-%d/%H:%M',
+                  verbose=False):
+
+    '''Retrieve segments of time between a start UTC
+    and end UTC centered on a given orbit number
+    and with a +/- of dorb.
+
+    If dorb is None or 0, then can be used to retrieve
+    time info of a point along the orbit segment, like
+    the apoapse or periapse.'''
+
+    if center_on == 'apoapse':
+        orbit_offset = 0.5
+    elif center_on == 'periapse':
+        orbit_offset = 0
+    else:
+        raise ValueError(
+            "Unrecognized argument for 'center_on' ({})."
+            " Try 'apopase' or 'periapse'.".format(center_on))
+
+    # if not using a dorb width, don't need start/end
+    # info:
+    if dorb == 0:
+        orbit_segment_name = ("center",)
+    else:
+        orbit_segment_name = ("start", "center", "end")
+
+    # Retrieve orbit ephemeris for this time period
+    eph = read_orbit_ephemeris(
+        data_directory,
+        start_date=start_time, end_date=end_time,
+        download_if_not_available=download_if_not_available)
+    print("Loaded ephemeris.")
+
+    # Get orbit info
+    orbnum = eph["orbnum"]
+    utc = eph["periapse_utc"]
+    unx = UTC_to_UNX(utc)
+    # print(utc)
+    # input()
+
+    # Start/end datetime and UNX to get orbits:
+    start_utc, n_days, end_utc = sanitize_date_inputs(
+        start_date=start_time, end_date=end_time)
+    end_utc = end_utc + dt.timedelta(days=1, seconds=-1)
+
+    # Get the unx at the boundaries
+    start_unx = UTC_to_UNX(start_utc)
+    end_unx = UTC_to_UNX(end_utc)
+
+    # Get orbit num at the start / end
+    start_orbnum = np.interp(start_unx, unx, orbnum)
+    end_orbnum = np.interp(end_unx, unx, orbnum)
+
+    # Get the orbits if the center of the apoapse, +/- dorb,
+    # is within the start/end
+    start_orb = np.floor(start_orbnum).astype('int')
+    if start_orbnum > (start_orb + (orbit_offset - dorb)):
+        start_orb += 1
+    end_orb = np.floor(end_orbnum).astype('int')
+    if end_orbnum < (end_orb + (orbit_offset + dorb)):
+        end_orb -= 1
+
+    n_orbits = (end_orb - start_orb) + 1
+
+    if verbose:
+        print("For UTC time range: {} - {}".format(
+            start_utc.strftime(t_format_string),
+            end_utc.strftime(t_format_string)))
+        print("Range of orbits: {} - {}".format(start_orbnum, end_orbnum))
+        print("Loaded orbits: {} - {}".format(start_orb, end_orb))
+
+        print("Retrieved UTC: {} - {}".format(start_orb, end_orb))
+        # print(orbnum)
+        print("# orbits: ", n_orbits)
+    orbits = np.linspace(start_orb, end_orb, n_orbits)
+    # print(orbits)
+
+    # Get the center of the desired orbit,
+    # and then get the time info before and after segment:
+    ctr_orbnum = orbits + orbit_offset
+    orbit_dict = {}
+    for t_at in orbit_segment_name:
+
+        if t_at == "start":
+            orbnum_i = (ctr_orbnum - dorb)
+        elif t_at == "end":
+            orbnum_i = (ctr_orbnum + dorb)
+        elif t_at == "center":
+            orbnum_i = ctr_orbnum
+        else:
+            raise IOError(
+                "t_at unrecognized, currently only "
+                "'start'/'end'/'center'.")
+
+        orb_unx_i = np.interp(orbnum_i, orbnum, unx)
+        orb_utc_i = UNX_to_UTC(orb_unx_i)
+        orb_text_i = [i.strftime(t_format_string) for i in orb_utc_i]
+
+        # Add to structure:
+        orbit_t_i =\
+            {"unx": orb_unx_i, "utc": orb_utc_i,
+             "num": orbnum_i, "str": orb_text_i}
+        orbit_dict[t_at] = orbit_t_i
+        # print(t_at)
+        # print(orbnum_i)
+        # print(orb_utc_i)
+        # input()
+
+    return orbit_dict
+
+
+def sort_segments_by_day(orbit_dicts,
+                         start_date=None, end_date=None, n_days=None,
+                         verbose=None, data_gaps=None):
+
+    '''Reorganize orbit segments into a data structure
+    that is ordered in increments of days. To handle orbit segments
+    that cross over a day boundary, a day is added.
+
+    This approach allows selective downloading of periods of days
+    based on orbit fragments, to reduce memory impact.
+
+    start, center, end: dicts containing num, utc, and unx.
+
+    '''
+    start_date, n_days, end_date = sanitize_date_inputs(
+        start_date=start_date, end_date=end_date)
+
+    # Pull the used parts of the orbit segment info:
+    start_orb_unx = orbit_dicts["start"]["unx"]
+    end_orb_unx = orbit_dicts["end"]["unx"]
+
+    # Identify and parse datagaps if supplied:
+    if data_gaps:
+        # Convert into datetime objects:
+        if isinstance(data_gaps[0], str):
+            data_gaps_dt = [[parse(j) for j in i.split("-")] for i in data_gaps]
+        elif isinstance(data_gaps[0], dt.datetime):
+            data_gaps_dt = data_gaps
+        else:
+            raise IOError(
+                "Unfamiliar datatype, only recognize data_gaps"
+                "as series of strings with a hyphen (-) to separate"
+                " the time range (e.g. '2021/10/9-2021/10/10') "
+                "or pairs of datetime objects.")
+
+    # Identify orbit segments that start on one day
+    # and end on the next:
+    day_index = 0
+    n_load_days = 1
+
+    # Arrays that contain the day indexing
+    day_index_arr = []
+    n_load_day_arr = []
+
+    # Initialize dictionary to dump sorted arrays into:
+    sorted_orbit_dicts = {}
+
+    while day_index < n_days:
+        # Get the datetime & UNX for the start of the day:
+        day_start_utc_i = start_date + dt.timedelta(days=day_index)
+        day_start_unx = UTC_to_UNX(day_start_utc_i)
+
+        # Check if the start is in the datagap. If so, skip to next.
+        day_end_utc = day_start_utc_i + dt.timedelta(
+            days=n_load_days, seconds=-1)
+        # if in_datagap(day_start_utc_i, data_gaps_dt):
+        # Need better way to handle last data on a datagap or first data
+        # will completely skip days containing a datagap
+        if in_datagap(day_start_utc_i, data_gaps_dt) or in_datagap(day_end_utc, data_gaps_dt):
+            day_index += 1
+            n_load_days = 1
+            continue
+
+        # if in_datagap(day_start_utc_i, data_gaps_dt):
+        #     day_index += 1
+        #     n_load_days = 1
+        #     continue
+
+        # if in_datagap(day_end_utc, data_gaps_dt):
+        #     n_load_days += 1
+        #     continue
+
+        # Get the index of the orbit segment after the day start:
+        i = np.searchsorted(start_orb_unx, day_start_unx)
+
+        # Datetime & UNX of last second of the day(s)
+        day_end_utc = day_start_utc_i + dt.timedelta(
+            days=n_load_days, seconds=-1)
+        day_end_unx = UTC_to_UNX(day_end_utc)
+
+        # Check if the subsequent day is in the datagap,
+
+        # Get the index of the first orbit segment start and end after
+        # the end of day
+        last_start_f = np.searchsorted(start_orb_unx, day_end_unx)
+        last_end_f = np.searchsorted(end_orb_unx, day_end_unx)
+
+        # If these indices are not the same, its because the first orbit
+        # segment that ends after the day ends started on the first day.
+        # Thus we want to load more days:
+        # But don't want to add a day if that day is in the data gap.
+        next_day = day_end_utc + dt.timedelta(seconds=1)
+        if last_start_f != last_end_f and not in_datagap(
+                next_day, data_gaps_dt):
+            n_load_days += 1
+            # print("added a day")
+            continue
+
+        f = last_start_f
+
+        if verbose:
+            # Conv to UTC times for intelligibility:
+            start_utc_first_orb_i = UNX_to_UTC(start_orb_unx[i])
+            end_utc_first_orb_i = UNX_to_UTC(end_orb_unx[i])
+            start_utc_last_orb_i = UNX_to_UTC(start_orb_unx[f - 1])
+            end_utc_last_orb_i = UNX_to_UTC(end_orb_unx[f - 1])
+            if f < len(start_orb_unx):
+                start_utc_first_excl_orb_i = UNX_to_UTC(start_orb_unx[f])
+                end_utc_first_excl_orb_i = UNX_to_UTC(end_orb_unx[f])
+
+            # print(start_orb_utc)
+            t_fmt = "%Y/%m/%d-%H:%M"
+            print(i, f, n_load_days)
+            print(last_start_f, last_end_f)
+            print("Start UTC of day: ",
+                  day_start_utc_i.strftime(t_fmt))
+            print("Start UTC of first orbit of period: ",
+                  start_utc_first_orb_i.strftime(t_fmt))
+            print("End UTC of first orbit of period: ",
+                  end_utc_first_orb_i.strftime(t_fmt))
+            print("Start UTC of last orbit:",
+                  start_utc_last_orb_i.strftime(t_fmt))
+            print("End UTC of last orbit:",
+                  end_utc_last_orb_i.strftime(t_fmt))
+            if f < len(start_orb_unx):
+                print("Start UTC of first excluded orbit:",
+                      start_utc_first_excl_orb_i.strftime(t_fmt))
+                print("End UTC of first excluded orbit:",
+                      end_utc_first_excl_orb_i.strftime(t_fmt))
+            else:
+                print("No excluded orbits, end of loaded ephemeris.")
+            # input()
+
+        for obs_type in orbit_dicts:
+            # Get the orbit information for the
+            # obs_type (e.g. start, end, center)
+            obs_data_i = orbit_dicts[obs_type]
+
+            # Get the equiv for the sorted, or
+            # create if not found:
+            if obs_type not in sorted_orbit_dicts:
+                sorted_obs_dict_i = {}
+            else:
+                sorted_obs_dict_i = sorted_orbit_dicts[obs_type]
+
+            # for each of the variables, select the
+            # subset and then append that as an array:
+            for var in obs_data_i:
+                # get the variable obs info (e.g. utc)
+                obs_var_i = obs_data_i[var]
+                # print(var)
+                # get the old sorted orb info,
+                # create the equivalent array if not in
+                # the sorted equivalent:
+                if var not in sorted_obs_dict_i:
+                    sorted_obs_var_i = []
+                else:
+                    sorted_obs_var_i = sorted_obs_dict_i[var]
+                # subset:
+                subset_obs_var_i = obs_var_i[i:f]
+                # append:
+                sorted_obs_var_i.append(subset_obs_var_i)
+                # reassign:
+                sorted_obs_dict_i[var] = sorted_obs_var_i
+
+            sorted_orbit_dicts[obs_type] = sorted_obs_dict_i
+
+        # orbsegment_ctr_time_utc_arr.append(center_utc[i:f])
+        # orbsegment_ctr_orbnum_arr.append(center_orb[i:f])
+        # orbsegment_start_unx_arr.append(start_orb_unx[i:f])
+        # orbsegment_end_unx_arr.append(end_orb_unx[i:f])
+
+        day_index_arr.append(day_index)
+        n_load_day_arr.append(n_load_days)
+
+        day_index += n_load_days
+        n_load_days = 1
+
+    if verbose:
+        print(day_index_arr)
+        print(n_load_day_arr)
+        print(sorted_orbit_dicts["center"]["num"])
+        # print([[i.strftime("%Y-%m-%d/%H:%M") for i in j]
+        #        for j in sorted_orbit_dicts["center"]["utc"]])
+        print(sorted_orbit_dicts["center"]["str"])
+
+    return day_index_arr, n_load_day_arr, sorted_orbit_dicts
