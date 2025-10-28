22a9705e09ab3de548d823a75d384ae6b61345d3
Rebecca Jolitz <rjolitz@berkeley.edu>
2025-10-28 13:23:47 -0700

    (sep_anc) Transferred SEP FOV MSO look direction and sun/mars-fov angle calculation functions to sep_anc, added contingency for no frames found. Still working to optimize the function for determining the frac of FOV obscured by Mars.

--- mavenpy/sep_anc.py
+++ mavenpy/sep_anc.py
@@ -1,10 +1,13 @@
+import time
 from collections.abc import Iterable
 
 import numpy as np
 import spiceypy
 
 from . import coordinates
-from .spice import dt_to_et
+from . import spice
+from . import mars_shape_conics
+from . import helper
 
 
 # Adaptation of mvn_sep_anc_fov_mars_fraction.pro,
@@ -24,13 +27,41 @@ SEP_fovs = {"1F": "SEP1A_front", "1R": "SEP1A_back",
             "2F": "SEP2A_front", "2R": "SEP2A_back"}
 
 
+def sep_mso_look_dir(time_UTC, sensor_number, look_direction):
+    """Rotate the look direction of a given MAVEN SEP sensor
+    into MSO coordinates."""
+
+    # # get ephemeris time:
+    # ephemeris_time = spice.dt_to_et(time_UTC)
+    # N = len(ephemeris_time)
+
+    look_direction = look_direction.lower()[:1]
+
+    if look_direction == "f":
+        fov_x = 1
+    elif look_direction == "r":
+        fov_x = -1
+    else:
+        raise ValueError(
+            "Look direction '{}' not recognized, "
+            "use 'r' or 'f' instead.".format(look_direction))
+
+    # Use wrapper from spice module (wraps spiceypy.pxform)
+    sep_mso = spice.pxform(
+        time_UTC, fov_x, 0, 0,
+        "MAVEN_SEP{}".format(sensor_number),
+        "MAVEN_MSO")
+
+    return sep_mso
+
+
 def get_fov_pixels(sep_sensor, sep_look_direction, pixel_width_deg=1.5):
 
     '''Returns a list of (x, y, z) coordinates that describe the boresight
     for a given SEP sensor-look direction.
     '''
 
-    sep_s_ld = "{}{}".format(sep_sensor, sep_look_direction)
+    sep_s_ld = "{}{}".format(sep_sensor, sep_look_direction.upper())
 
     naif_code = SEP_fov_NAIF_ID[SEP_fovs[sep_s_ld]]
 
@@ -82,8 +113,107 @@ def get_fov_pixels(sep_sensor, sep_look_direction, pixel_width_deg=1.5):
     return fov_x, fov_y, fov_z, theta_centers_2d, phi_centers_2d
 
 
+def fov_target_angle(sensor, look_direction, time_utc, target,
+                     time_unix=None,
+                     MAVEN_position_spline=None, n_spline_points=300,
+                     calculate_position=False,
+                     verbose=False):
+    """
+    target: solar system object(s) to take angle wrt, e.g.
+        'sun' or 'Mars'
+    calculate_position: Boolean to calculate the MAVEN
+        position at each point instead of using spline
+        (slower, but more precise)
+    """
+
+    # Turn target into iterable, so can return angles for each:
+    if isinstance(target, str):
+        target = (target,)
+
+    # Convert UTC time to Unix, if not supplied:
+    if time_unix is None:
+        time_unix = helper.UTC_to_UNX(time_utc)
+
+    # Get the MSO coordinates of the center of the boresight:
+    if verbose:
+        print("Get FOV coordinates in MSO...")
+        time_init = time.time()
+    fov_mso_unit_vec = sep_mso_look_dir(time_utc, sensor, 'f')
+    if verbose:
+        print("Done: {} s".format(time.time() - time_init))
+
+    fov_angle_dict = {}
+    for target_i in target:
+        target_i_lc = target_i.lower()
+        if target_i_lc == 'mars':
+            # Get the position vector in MSO, which is needed
+            # for Mars-FOV angle:
+            if calculate_position:
+                # Calculate spacecraft position using spice for
+                # each increment in time:
+                if verbose:
+                    print("No position supplied, calculating MAVEN "
+                          "position in MSO "
+                          "(will be slow, use spline if speed "
+                          "preferred)...")
+                    time_init = time.time()
+                sc_x, sc_y, sc_z = spice.MAVEN_position(time_utc)
+                if verbose:
+                    print("Done: {} s".format(time.time() - time_init))
+            else:
+                # Use the spline to eval the spacecraft position:
+                if not MAVEN_position_spline:
+                    if verbose:
+                        print(
+                            "No position spline supplied, calculating"
+                            " spline with {} points...".format(
+                                n_spline_points))
+                        time_init = time.time()
+                    # If no spline provided and not calculating position,
+                    # first get the position (N=300 points per day
+                    # gets sub-km precision)
+                    time_utc_m, time_unx_m, xm, ym, zm = spice.load_MAVEN_position(
+                        time_utc[0], end_date=time_utc[-1],
+                        n_sample_points=n_spline_points, frame="MAVEN_MSO")
+                    MAVEN_position_spline = mars_shape_conics.cartesian_spline(
+                        time_unx_m, xm, ym, zm)
+                    if verbose:
+                        print("Done: {} s".format(time.time() - time_init))
+
+                # Use the spline to eval the spacecraft position
+                # at the times the FOV target angle is eval'ed:
+                sc_x = MAVEN_position_spline[0](time_unix)
+                sc_y = MAVEN_position_spline[1](time_unix)
+                sc_z = MAVEN_position_spline[2](time_unix)
+
+            # Get r and calculate unit vector.
+            sc_r = np.sqrt(sc_x**2 + sc_y**2 + sc_z**2)
+            mars_unit = np.zeros(shape=(sc_x.size, 3))
+            mars_unit[:, 0] = -sc_x/sc_r
+            mars_unit[:, 1] = -sc_y/sc_r
+            mars_unit[:, 2] = -sc_z/sc_r
+
+            fov_dot_unit_vector = np.sum(fov_mso_unit_vec * mars_unit, axis=1)
+
+        elif target_i_lc == 'sun':
+            # MSO x axis is sun-pointing, so unit_vector_sun dot
+            # fov_mso_unit_vector is just the x coordinate of the
+            # fov_mso_unit_vector
+            fov_dot_unit_vector = fov_mso_unit_vec[:, 0]
+
+        fov_angle_i = np.degrees(np.arccos(fov_dot_unit_vector))
+        fov_angle_dict[target_i_lc] = fov_angle_i
+
+    return fov_angle_dict
+
+
 def fraction_Mars_in_FOV(sensor, look_direction, time_utc,
-                         pixel_width_deg=1.5):
+                         pixel_width_deg=1.5,
+                         use_pointing_info=False,
+                         MAVEN_position_spline=None,
+                         n_spline_points=300,
+                         fov_angle=None,
+                         fov_angle_threshold=105):
     '''Returns the fraction of the field of view
     of a given SEP sensor is taken up by (sunlit) Mars
 
@@ -91,7 +221,8 @@ def fraction_Mars_in_FOV(sensor, look_direction, time_utc,
     '''
 
     # Convert to ephemeris time:
-    ephemeris_time = dt_to_et(time_utc)
+    ephemeris_time = spice.dt_to_et(time_utc)
+    unix_time = helper.UTC_to_UNX(time_utc)
 
     # Spice frame that all SEP boresight vectors are in:
     frame = "MAVEN_SEP{}".format(sensor)
@@ -115,18 +246,40 @@ def fraction_Mars_in_FOV(sensor, look_direction, time_utc,
     mars_in_fov = np.zeros(shape=(n_time, n_fov_vec))
     sunlit_mars_in_fov = np.zeros(shape=(n_time, n_fov_vec))
 
+    # generate an index array marking where we eval FOV
+    eval_ephemeris_index = np.where(unix_time == unix_time)[0]
+    # print(eval_ephemeris_index)
+    # input()
+    if use_pointing_info:
+        # Calculate the FOV-Mars angle if not supplied:
+        if fov_angle is None:
+            fov_angle_dict = fov_target_angle(
+                sensor, look_direction, time_utc, ('mars',),
+                MAVEN_position_spline=MAVEN_position_spline,
+                n_spline_points=n_spline_points,
+                calculate_position=False)
+            fov_angle = fov_angle_dict['mars']
+        potential_obscure = np.where(fov_angle < fov_angle_threshold)[0]
+        eval_ephemeris_index = eval_ephemeris_index[potential_obscure]
+
     # Disables the error when no intercept:
     with spiceypy.no_found_check():
-        for j, et_j in enumerate(ephemeris_time):
+        # for j, et_j in enumerate(ephemeris_time):
+        for j in eval_ephemeris_index:
+            et_j = ephemeris_time[j]
             for i, fov_xyz_i in enumerate(zip(fov_x, fov_y, fov_z)):
                 # print(i, fov_xyz_i)
                 # Just pxform takes 6 seconds/100 time segments:
                 # sincpt (Surface intercept):
                 # Takes 6 seconds/100 time segments
-                spoint, trgepc, srfvec, intercept = spiceypy.sincpt(
-                    'Ellipsoid', 'Mars', et_j,
-                    'IAU_MARS', 'NONE', 'MAVEN', frame,
-                    fov_xyz_i)
+                try:
+                    spoint, trgepc, srfvec, intercept = spiceypy.sincpt(
+                        'Ellipsoid', 'Mars', et_j,
+                        'IAU_MARS', 'NONE', 'MAVEN', frame,
+                        fov_xyz_i)
+                except spiceypy.utils.exceptions.SpiceNOFRAMECONNECT:
+                    print("Missing Spice info for {}, skipping.".format(time_utc[j]))
+                    break
 
                 mars_in_fov[j, i] = intercept
 
